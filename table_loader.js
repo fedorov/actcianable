ee4196d8df38a94d74a6cbfd2d0bfdea1ef0eb3b
Andrey Fedorov <andrey.fedorov@gmail.com>
2020-12-29 10:36:21 -0500

    edit

--- table_loader.js
+++ table_loader.js
@@ -1,7 +1,14 @@
+$.getJSON("output/collections.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
+
+/*
+var $table = $('#collectionsTable');
+
 const collections = require("./output/collections.json");
 
 $(function () {
-    $('#table').bootstrapTable({
+    $('#collectionsTable').bootstrapTable({
         data: collections
     });
-});
+});*/
