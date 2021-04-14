154ffabba71970fd0ed594bd35bb380ed8e2d79b
Andrey Fedorov <andrey.fedorov@gmail.com>
2021-04-14 17:37:22 -0400

    update status view

--- scrapers/make_views.py
+++ scrapers/make_views.py
@@ -41,7 +41,12 @@ with open('output/image_analyses_view.json', 'w') as f:
   df_json = json.dumps(df_dict, indent=2)
   f.write(df_json)
 
-merged_df = collections_df.merge(status_df, on=["Collection"], how="left")[["Collection", "DOI", "Access", "some_date", "is_excluded"]]
+merged_df = collections_df.merge(status_df[["Collection", "some_date","is_excluded"]], on=["Collection"], how="left") [["Collection", "DOI", "Access", "some_date", "is_excluded"]]
+merged_df["is_excluded"] = merged_df["is_excluded"].astype(bool)
+
+
+print(merged_df.columns)
+
 with open('output/status_view.json', 'w') as f:
   df_dict = merged_df.to_dict(orient='records')
   df_json = json.dumps(df_dict, indent=2)
