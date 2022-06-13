1714777fe744ce94c14803b4503dd57a9fee6c9f
George <george@butter.local>
2022-06-13 09:22:37 -0700

    update clinical dat scraping

--- scrapers/get_clinical.py
+++ scrapers/get_clinical.py
@@ -5,12 +5,73 @@ import backoff
 import json
 import os
 import hashlib
+from datetime import datetime
 
 @backoff.on_exception(backoff.expo,
                       requests.exceptions.RequestException,
                       max_time=60)
 def get_url(url):#, headers):
   return requests.get(url) #, headers=headers)
+
+
+
+def fetchClinical(collection_name, collection_href, data_formats, filenms):
+  colrequest = requests.get(collection_href, timeout=30)
+  content = colrequest.content
+  collection_html = BeautifulSoup(content, 'html.parser')
+  data_access_rows = collection_html.find('div',{'name':'Data Access'}).find('tbody').find_all('tr')
+  some_external=False
+  access_error=True
+  some_internal=False
+  for da_row in data_access_rows:
+    tds= da_row.find_all('td')
+    if (len(tds)>0):
+      data_type =tds[0].text.strip()
+      # data_type reported as 'clinical' in the 'Data Type' column means there is clinical data
+
+      if ( (data_type.lower().find('clinical')>-1) or  (('alternate' in current_clinical[collection_name]) and (data_type in current_clinical[collection_name]['alternate']))):
+        some_internal = True
+        #clinical_found = True
+        #print(collection_name+" "+data_type)
+        hrefArr = tds[1].find_all('a', href=True)
+        for href in hrefArr:
+          download_file_found_in_row=False
+          filenm=''
+          url = href.attrs['href']
+          purl = urlparse(url)
+          #add tcia hostname to internal links
+          if purl.hostname is None:
+            url = tcia_protocol+tcia + url
+            purl = urlparse(url)
+          # some download urls links not on tcia
+          if (purl.hostname.find(tcia) == -1):
+            some_external = True
+          else:
+            try:
+              print(url)
+              head_info = requests.head(url, timeout=5)
+              # if the 'download' link does not include a 'Content-Disposition' header this likely means that the link does NOT resolve to a downloadable file
+              if ('Content-Disposition' in head_info.headers) and (head_info.headers['Content-Disposition'].find('filename=') > -1):
+                  filenm = head_info.headers['Content-Disposition'].split("filename=")[1].replace('"', '')
+                  ext = os.path.splitext(filenm)[1].lower()
+                  data_formats.add(ext)
+                  resp = requests.get(url)
+                  md5 = hashlib.md5(resp.content).hexdigest()
+                  filenms.append('<a href="' + url + '">' + filenm + '</a>: ' + md5)
+                  dirname = collection_name.replace('/', '_')
+                  path = 'output/clinical_files/' + dirname
+                  if not os.path.exists(path):
+                      os.makedirs(path)
+                  with open(path + '/' + filenm, 'wb') as f:
+                      f.write(resp.content)
+              else:
+                access_error = True
+            except:
+              access_error = True
+  if not some_internal:
+    some_external = True
+  return([access_error,some_external])
+
 
 notes={}
 try:
@@ -23,9 +84,8 @@ try:
 except IOError:
     print("clinical notes file not found")
 
-
-
-tcia= 'https://wiki.cancerimagingarchive.net'
+tcia_protocol='https://'
+tcia= 'wiki.cancerimagingarchive.net'
 URL = 'https://www.cancerimagingarchive.net/collections/'
 page = get_url(URL)
 soup = BeautifulSoup(page.content, "html.parser")
@@ -33,7 +93,7 @@ tableb = soup.find(id="tablepress-9").find('tbody')
 rows = tableb.find_all("tr")
 outTable = []
 
-header = "Collection,DOI,Format,External,Download,Filenames,Notes".split(",")
+header = "Collection,DOI,External,Format,Filenames,Notes".split(",")
 
 for row in rows:
   trow = {}
@@ -44,103 +104,36 @@ for row in rows:
   if collection_href.startswith('//'):
       collection_href = 'https:' + collection_href
 
-  colrequest = requests.get(collection_href, timeout=30)
-  content = colrequest.content
-  collection_html = BeautifulSoup(content, 'html.parser')
-  data_access_rows = collection_html.find('div',{'name':'Data Access'}).find('tbody').find_all('tr')
-  clinical_found = False
-  some_clinical_downloadable = False
-  some_clinical_not_downloadable = False
-  some_clinical_external = False
-  data_formats = set()
-  filenms = []
-  for da_row in data_access_rows:
-    tds= da_row.find_all('td')
-    if (len(tds)>0):
-      data_type =tds[0].text.strip()
+  collection_supporting_col = row.find('td',{'class':'column-7'})
+  collection_supporting = collection_supporting_col.text.strip()
 
-      # data_type reported as 'clinical' in the 'Data Type' column means there is clinical data
-      if (data_type.lower().find('clinical')>-1):
+  if ('Clinical' in collection_supporting):
+    if not (collection_name in current_clinical):
+        current_clinical[collection_name]={}
+        current_clinical[collection_name]['notes'] ='new collection detected as of '+str(datetime.now().date())
+        notes[collection_name] = current_clinical[collection_name]['notes']
 
-        clinical_found = True
-        #print(collection_name+" "+data_type)
-        hrefArr = tds[1].find_all('a', href=True)
-        for href in hrefArr:
-          download_file_found_in_row=False
-          filenm=''
-          url = href.attrs['href']
-          purl = urlparse(url)
-          #add tcia hostname to internal links
-          if purl.hostname is None:
-            url = tcia + url
-            purl = urlparse(url)
-          # some download urls links not on tcia
-          if (purl.hostname.find('cancerimagingarchive') == -1):
-            some_clinical_external = True
-          # check header before trying to download file
-          try:
-            print(url)
-            head_info = requests.head(url, timeout=5)
-            successReq = True
-          except:
-              successReq = False
-          # if the 'download' link does not include a 'Content-Disposition' header this likely means that the link does NOT resolve to a downloadable file but to a web portal or json content
-          if (successReq) and ('Content-Disposition' in head_info.headers) and (head_info.headers['Content-Disposition'].find('filename=') > -1):
-            some_clinical_downloadable = True
-            download_file_found_in_row = True
-            filenm = head_info.headers['Content-Disposition'].split("filename=")[1].replace('"', '')
-            ext = os.path.splitext(filenm)[1].lower()
-            data_formats.add(ext)
-          # clinical data may in a json resource
-          elif (successReq) and ('Content-Type' in head_info.headers) and (head_info.headers['Content-Type'].find('json')>-1):
-            some_clinical_downloadable = True
-            download_file_found_in_row = True
-            filenm = url.replace('https://','')
-            filenm = filenm.replace('http://', '')
-            filenm = filenm.replace('/', '_')
-            data_formats.add('.json')
-          if download_file_found_in_row:
-            resp = requests.get(url)
-            md5=hashlib.md5(resp.content).hexdigest()
-            filenms.append('<a href="'+url+'">'+filenm+'</a>: '+md5)
-            dirname=collection_name.replace('/','_')
-            path='output/clinical_files/'+dirname
-            if not os.path.exists(path):
-                os.makedirs(path)
-            with open(path+'/'+filenm, 'wb') as f:
-              f.write(resp.content)
-          else:
-              some_clinical_not_avail = True
-
-  if clinical_found:
+    data_formats=set()
+    filenms=[]
+    some_external=False
+    [access_error, some_external] = fetchClinical(collection_name, collection_href, data_formats, filenms)
     data_formats_list=list(data_formats)
     data_formats_list.sort()
     data_format_str= ", ".join(data_formats_list)
     filenms.sort()
     filenm_str= ", ".join(filenms)
-    avail=""
-    if some_clinical_downloadable:
-      if some_clinical_not_downloadable:
-        avail="partial"
-      else:
-        avail="yes"
-    else:
-      avail="no"
+
     trow[header[0]] = collection_name
     trow[header[1]] = collection_href
-    trow[header[2]]=data_format_str
-    trow[header[3]]=some_clinical_external
-    trow[header[4]] = avail
-    trow[header[5]] = filenm_str
-    if collection_name in notes:
-      trow[header[6]] = notes[collection_name]
-    else:
-      trow[header[6]] = ''
-
-  if len(trow):
+    trow[header[2]] = some_external
+    trow[header[3]]=data_format_str
+    trow[header[4]] = filenm_str
+    try:
+      trow[header[5]] = notes[collection_name]
+    except:
+      pass
     outTable= outTable + [trow]
 
-#print(len(outTable))
 with open("output/clinical_collections.json", "w") as f:
   f.write(json.dumps(outTable, indent=2))
 
